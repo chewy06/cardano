61a4c2e8aa2a13b7f0066661d93791dfaf1407eb
Jesus Vivar <chewy06ahc@gmail.com>
2022-01-02 23:50:59 -0800

    Updated sumStr due to base case

--- Haskell/sumStr.hs
+++ Haskell/sumStr.hs
@@ -1,6 +1,13 @@
--- Takes in 2 string number and return the sum as String
+{- Takes in 2 string number and return the sum as String
 sumStr str1 str2 = show n3
     where
         n1 = read str1 :: Int
         n2 = read str2 :: Int
         n3 = n1 + n2
+-}
+
+sumStr str1 str2 = show $ readStr str1 + readStr str2
+
+readStr "" = 0
+readStr  s = read s :: Int
+
